de4f8b8e420e72e472fb8710452d18dc27276be7
ndahisaac <92581588+ndahisaac@users.noreply.github.com>
2022-05-03 11:27:13 +0400

    Add files via upload
    This is ansible setup with dynamic setup

---
+++ Ansible.sh
@@ -0,0 +1,89 @@
+
+# STATIC INVENTORY
+# Setting up Passwordsless Authentication/SSh Key Base Authentication 
+ =====================================================================
+ sudo useradd ansible
+ passwd ansible
+ # Vi into the following file and edit the following lines
+ sudo cp /etc/ssh/sshd_config
+ sudo vi /etc/ssh/sshd_config
+:/PasswordAuthentication :yes
+:/PermitRootLogin
+x!
+sudo systemctl sshd
+==================================
+The about method did not work : I will research on it 
+=================================
+
+=====================================================================================================================================================
+# DYNAMIC INVENTORY
+#Setting up dynamic inventory  in ubuntu server 
+# setting up ansible with one master and two worker-Nodes
+#On Master run all commands a regular user
+sudo hostnamectl set-hostname "server name"
+exec bash
+
+# update ubuntu
+sudo apt update -y
+# Check the version of your linux
+uname -a 
+# install ansible
+sudo apt install -y ansible
+# install python
+sudo apt-get install python3 -y
+sudo apt-get install python3-pip -y
+sudo pip3 install boto3
+sudo apt-get install python-boto3
+# install boto 3 $ awscli is not required for the setup 
+sudo pip install boto3
+ install awscli
+# uncomment  sudo_user =root the ansible config file 
+sudo cp  /etc/ansible/ansible.cfg  /etc/ansible/ansible.cfg.backup 
+sudo vi /etc/ansible/ansible.cfg 
+# vi into your key.pem and paste in the master
+sudo vi "key.pem"
+chmod 400 key.pem
+#in case you ran the above commands a root run the following command 
+chown ubuntu:uobuntu "key.pem"
+#copy the below files :
+aws s3 cp s3://code-sharing-bucket/jespo/ansible/ec2.py .
+aws s3 cp s3://code-sharing-bucket/jespo/ansible/ec2.ini .
+# Configure credential for the master server 
+export AWS_ACCESS_KEY_ID='AKIAYSHKVTCPW7W73H3U'
+export AWS_SECRET_ACCESS_KEY='xxxxxxxxxxxxxx'
+https://raw.githubusercontent.com/ansible/ansible/stable-1.9/plugins/inventory/ec2.ini
+https://raw.githubusercontent.com/ansible/ansible/stable-1.9/plugins/inventory/ec2.py
+# Give both files executable permisssions
+chmod +x ec2.ini
+chmod +x ec2.py
+# run the following command
+ssh-agent bash
+# add pem key to ubuntu home directory
+ssh-add /home/ubuntu/key.pem
+# export the followinig command 
+export EC2_INI_PATH=/home/ubuntu/ec2.ini
+export ANSIBLE_HOSTS=/home/ubuntu/ec2.py
+# make sure you name your slaves as Clients
+export EC2_INSTANCE_FILTERS='tag:Name=Client*'
+# you the following script to scan your environment 
+# the below command is required before the inventory can be collected
+pip install boto
+./ec2.py
+# go into remote notes and create a file call demo.txt and the command is an ad-hoc command
+ansible -m file -b -a "path=/home/ubuntu/demo2.txt state=touch" -i ec2.py ec2
+#vi into  /etc/ansible/ansible.cfg and uncomment the following lines 
+sudo vi /etc/ansible/ansible.cfg 
+host_key_checking = False”
+# the following command will update all the available clients -b (become)
+ansible -m yum -b -a "name=ntp state=absent" -i ec2.py ec2
+
+#ANSIBLE AD-HOC COMMANDS 
+
+
+ansible -m apt -b -a "name=* state=latest" -i ec2.py ec2ansible -m file -b -a "path=/home/ubuntu/demo2.txt state=touch" -i ec2.py ec2ansible -m copy -b -a "src=/home/ubuntu/test1 dest=/home/ubuntu" -i ec2.py ec2ansible -m user -b -a "name=user1 state=present" -i ec2.py ec2ansible -m user -b -a "name=user1 state=present" -i ec2.py ec2ansible -m user -b -a "name=group1" -i ec2.py ec2ansible -m user -b -a "name=user1 group=group1" -i ec2.py ec2ansible -m user -b -a "name=ntp state=absent" -i ec2.py ec2ansible -m user -b -a "name=httpd state=absent" -i ec2.py ec2ansible -m user -b -a "name=httpd state=started" -i ec2.py ec2
+
+
+
+
+
+
